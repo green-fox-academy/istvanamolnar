3c56d392c952435f901a4cf22f4689fc15b6bacf
istvanamolnar <istvan0988@gmail.com>
2019-04-05 14:45:43 +0200

    code simplified

--- Foundation/Week1/typescript/substring.ts
+++ Foundation/Week1/typescript/substring.ts
@@ -8,10 +8,30 @@ function substr(str: string, keyword: string) : number {
     return str.indexOf(keyword) ? str.indexOf(keyword) : -1;
 }
 
+function substr2(str: string, keyword: string) {
+    let i = 0; 
+    while (i < (str.length - keyword.length)) {
+            if (keyword[0] === str[i]) {
+                //str.slice();
+                let sliced: string = "";
+                for (let j = i; j < (i + keyword.length); j++) {
+                    sliced += str[j];
+                }
+                if (sliced === keyword) {
+                    return i;
+                } else {
+                    i++;
+                }
+            } else {
+                i++;
+            }
+    }
+    return -1;
+}
 //  Example
 
 // should print: `17`
-console.log(substr("this is what I'm searching in", "searching"));
+console.log(substr2("this is what I'm searching in", "searching"));
 
 // should print: `-1`
-console.log(substr("this is what I'm searching in", "not"));
+console.log(substr2("this is what I'm searching in", "not"));
