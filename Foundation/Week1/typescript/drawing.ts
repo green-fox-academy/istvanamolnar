e0ca488c282c9a975ddbc4fed43382bdb67f134d
istvanamolnar <istvan0988@gmail.com>
2019-04-05 10:19:15 +0200

    new function

--- Foundation/Week1/typescript/drawing.ts
+++ Foundation/Week1/typescript/drawing.ts
@@ -13,14 +13,14 @@ function repeat(character: string, string: string, num: number) {
 // Write a program that draws a pyramid
 function drawPyramid(numberOfLines:number) {
     let space: string = "";
-    let stars: string = "*";
+    let star: string = "*";
     let counter2: number = numberOfLines;
     for (let i = numberOfLines; i > 0; i--) {
-        console.log(repeat(" ", space, i), stars);
-        stars += "**";
+        console.log(repeat(" ", space, i), star);
+        star += "**";
     }
 }
-
+//drawPyramid(6);
 
 // Write a program that draws a square like this:
 function drawSquare(numberOfLines: number) {
@@ -31,7 +31,17 @@ function drawSquare(numberOfLines: number) {
     }
     console.log(repeat("%", space, numberOfLines));
 }
+//drawSquare(8);
 
 
-drawPyramid(6);
-drawSquare(8);
+// Write a program that draws a square with a diagonal
+function drawSquareWithDiagonal(numberOfLines: number) {
+    let space: string = "";
+    let percent: string = "%";
+    console.log(repeat("%", space, numberOfLines));
+    for (let i = 0; i < numberOfLines - 2; i++) {
+        console.log("%" + repeat(" ", space, i) + "%" + repeat(" ", space, numberOfLines - 3 - i) + "%");
+    }
+    console.log(repeat("%", space, numberOfLines));
+}
+//drawSquareWithDiagonal(10);
