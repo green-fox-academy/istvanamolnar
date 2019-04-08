0b01f4fb3a77b851ab387a980d18bfdf8482445c
istvanamolnar <istvan0988@gmail.com>
2019-04-08 14:07:31 +0200

    lighting talk

---
+++ Foundation/Week2/LightingTalk-Rubik/rubik.ts
@@ -0,0 +1,61 @@
+'use strict'
+
+function solveTheCube(size: string) {
+    solveTheCross(white);
+    solveTheTopCorners(white);
+    solveTheMiddleLayer();
+    solveTheBottom(yellow);
+}
+
+function solveTheCross(color: string): any {
+    putFirstPieceToPlace();
+    putSecondPieceToPlace();
+    putThirdPieceToPlace();
+    putFourthPieceToPlace();
+}
+
+function solveTheTopCorners(color: string) {
+    putThePieceToTheRightPlace();
+    if ("white is on the left") {
+        perform("D  L  D'  L'");
+    } else if ("white is on the right") {
+        perform("D'  L'  D  L");
+    } else /* white is facing to the bottom*/ {
+        turnTheCubeUpsideDown();
+        perform("sexy move".repeat(3));
+    }
+}
+
+function solveTheMiddleLayer() {
+    putThePieceToTheRightPlace();
+    if ("if the block goes to the right") {
+        perform("D'  R'  D  R'  D  F  D'  F'");
+    } else { // if the block goes to the left
+        perform("D  L  D'  L'  D'  F'  D  F");
+    }
+}
+
+function solveTheBottom() {
+    turnTheCubeUpsideDown();
+
+    flipTheEdgePieces() {
+        perform("U  R'  U'  R  U  R  U'  R'");
+    }
+    flipCornerPieces() {
+        perform("R' B' R B") && turnTop();
+    }
+    rotateEdgePieces() {
+        perform("R  U'  L'  U  R'  2U  L  U'  L'  2U  L")
+    }
+    rotateCornerPieces() {
+        perform("2R  2F'  R'  B'  R  2F  R'  B  R'");
+    }
+
+}
+
+solveTheCube('3x3x3');
+
+https://ruwix.com/the-rubiks-cube/advanced-cfop-fridrich/orient-the-last-layer-oll/
+
+
+muscle memory
