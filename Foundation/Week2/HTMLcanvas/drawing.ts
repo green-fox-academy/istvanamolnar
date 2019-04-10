9ad099640a8dcfd6e3a337952e55b1b0edb560ce
istvanamolnar <istvan0988@gmail.com>
2019-04-10 10:36:28 +0200

    drawing example

--- Foundation/Week2/HTMLcanvas/drawing.ts
+++ Foundation/Week2/HTMLcanvas/drawing.ts
@@ -4,7 +4,7 @@ const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 canvas.height = 600;
 canvas.width = 600;
 
-const ctx = canvas.getContext('2d');
+const ctx: any = canvas.getContext('2d');
 
 //house
 ctx.fillStyle = 'grey';
