d6e24f47822d17d8e7930ebfde19a81123c4f1b3
istvanamolnar <istvan0988@gmail.com>
2019-04-17 12:27:56 +0200

    class practice

---
+++ Foundation/Week3/class/Animal.ts
@@ -0,0 +1,35 @@
+'use strict';
+
+// Create an Animal class
+// Every animal has a hunger value, which is a whole number
+// Every animal has a thirst value, which is a whole number
+// when creating a new animal object these values are created with the default 50 value
+// Every animal can eat() which decreases their hunger by one
+// Every animal can drink() which decreases their thirst by one
+// Every animal can play() which increases both by one
+
+class Animal {
+  hunger: number = 50;
+  thirst: number = 50;
+
+  eat(): void {
+    this.hunger--;
+  }
+
+  drink(): void {
+    this.thirst--;
+  }
+
+  play(): void {
+    this.hunger++ && this.thirst++;
+  }
+}
+
+const Dog: Animal = new Animal();
+
+Dog.eat();
+Dog.eat();
+Dog.play();
+Dog.play();
+
+console.log(Dog);
--- Foundation/Week3/class/BlogPost.ts
+++ Foundation/Week3/class/BlogPost.ts
@@ -29,8 +29,8 @@ class BlogPost {
 
 const longString: string = 'Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.'
 
-const firstPost = new BlogPost('John Doe', 'Lorem Ipsum', 'Lorem ipsum dolor sit amet.', '2000.05.04.');
-const secondPost = new BlogPost('Tim Urban', 'Wait but why', 'A popular long-form, stick-figure-illustrated blog about almost everything.', '2010.10.10.');
-const thirdPost = new BlogPost('William Turton', 'One Engineer Is Trying to Get IBM to Reckon With Trump', longString, '2017.03.28.');
+const firstPost: BlogPost = new BlogPost('John Doe', 'Lorem Ipsum', 'Lorem ipsum dolor sit amet.', '2000.05.04.');
+const secondPost: BlogPost = new BlogPost('Tim Urban', 'Wait but why', 'A popular long-form, stick-figure-illustrated blog about almost everything.', '2010.10.10.');
+const thirdPost: BlogPost = new BlogPost('William Turton', 'One Engineer Is Trying to Get IBM to Reckon With Trump', longString, '2017.03.28.');
 
 //console.log(firstPost, secondPost, thirdPost);
