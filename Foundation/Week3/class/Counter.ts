905257ab78a851c1b488e98892d7f9dd818b9762
istvanamolnar <istvan0988@gmail.com>
2019-04-17 13:15:19 +0200

    class practice

---
+++ Foundation/Week3/class/Counter.ts
@@ -0,0 +1,42 @@
+'use strict';
+
+
+// Create a Counter class
+// which has an integer property
+// when creating it should have a default value 0 or we can specify it when creating
+// we can add(number) to increase the counter's value by a whole number
+// or we can add() without parameters just increasing the counter's value by one
+// and we can get() the current value as string
+// also we can reset() the value to the initial value
+
+class Counter {
+  integer: number;
+  initial: number;
+
+  constructor(integer: number = 0) {
+    this.integer = integer;
+    this.initial = integer;
+  }
+
+  add(num: number = 1): void {
+    this.integer += num;
+  }
+
+  get(): any {
+    return this.integer.toString();
+  }
+
+  reset(): void {
+    this.integer = this.initial;
+  }
+}
+
+const Integer: Counter = new Counter(5000)
+const Integer2: Counter = new Counter();
+
+Integer.add();
+Integer.add(500);
+Integer2.add(40);
+Integer2.reset();
+
+console.log(Integer, Integer2);
