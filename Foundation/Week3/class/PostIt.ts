fa1eeb6be72f1c99f87e3c435d396c8106aa4e61
istvanamolnar <istvan0988@gmail.com>
2019-04-17 11:53:22 +0200

    class practice

---
+++ Foundation/Week3/class/PostIt.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+class PostIt {
+  backgroundColor: string;
+  textOnIt: string;
+  textColor: string;
+
+  constructor (backgroundColor: string, textOnIt: string, textColor: string) {
+    this.backgroundColor = backgroundColor;
+    this.textOnIt = textOnIt;
+    this.textColor = textColor;
+  }
+}
