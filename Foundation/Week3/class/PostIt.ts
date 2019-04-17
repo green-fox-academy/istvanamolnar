d675b959b8f860b74a957e2a36543be8e0fa4b51
istvanamolnar <istvan0988@gmail.com>
2019-04-17 11:56:27 +0200

    class practice

--- Foundation/Week3/class/PostIt.ts
+++ Foundation/Week3/class/PostIt.ts
@@ -11,3 +11,11 @@ class PostIt {
     this.textColor = textColor;
   }
 }
+
+// Create a few example post-it objects:
+// an orange with blue text: "Idea 1"
+// a pink with black text: "Awesome"
+// a yellow with green text: "Superb!"
+const firstPostIt: object = new PostIt('orange', 'Idea 1', 'blue');
+const secondPostIt: object = new PostIt('pink', 'Awesome', 'black');
+const thirdPostIt: object = new PostIt('yellow', 'Superb!', 'green');
