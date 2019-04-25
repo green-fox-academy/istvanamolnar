8d9ebdfbeaf887a01fab8166a8c0e0fb3659aafc
istvanamolnar <istvan0988@gmail.com>
2019-04-25 19:16:46 +0200

    (feat)  code splitted to smaller units

--- Foundation/Week4/Testing/CowsAndBulls/CowsAndBulls.ts
+++ Foundation/Week4/Testing/CowsAndBulls/CowsAndBulls.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-function randomArray(): number[] { //generates the array of the secret numbers
+function secretArray(): number[] { //generates the array of the secret numbers
   let arrayOfNumbers: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   let cabArray: number[] = [];
   for (let i: number = 0; i < 4; i++) {
@@ -11,11 +11,19 @@ function randomArray(): number[] { //generates the array of the secret numbers
   }
   return cabArray;
 }
-const secret: number[] = randomArray();
-let guess:  number[] = [1, 2, 3, 4];
+const secret: number[] = secretArray();
+console.log(secret);
 
+let prevGuess: string = '';
+function guesser(): number[] { //takes and prints guesses
+  let readlineSync = require('readline-sync');
+  let guess: number[] = readlineSync.question('Enter four single digit integers: ').split('').map((x:string) => parseInt(x));
+  prevGuess += '\n' + guess;
+  return guess;
+}
 
-function guessNumber(guess: number[]): void {
+function cattleCounter(secret: number[]): any {
+  const guess: number[] = guesser();
   let Cattle: any = {
     'bull': 0,
     'cow': 0
@@ -29,7 +37,8 @@ function guessNumber(guess: number[]): void {
       }
     }
   }
-  console.log(secret, guess, Cattle);
+  console.log(`${prevGuess}   Bulls: ${Cattle.bull}, Cows: ${Cattle.cow}`)
+  return Cattle.bull === 4 ? console.log('GG WP') : cattleCounter(secret);
 }
 
-guessNumber(guess);
+cattleCounter(secret);
