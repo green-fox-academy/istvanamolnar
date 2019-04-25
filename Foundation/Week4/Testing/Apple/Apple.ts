2250e106dc841c1f28f4ba4eccfa5022479c7a23
istvanamolnar <istvan0988@gmail.com>
2019-04-25 11:32:55 +0200

    test practice

--- Foundation/Week4/Testing/Apple/Apple.ts
+++ Foundation/Week4/Testing/Apple/Apple.ts
@@ -1,7 +1,12 @@
 'use strict';
 
 export class Apple {
+
   getApple(): string {
     return 'apple';
+  }
+
+  sum(arrayOfNumbers: number[]): number {
+    return arrayOfNumbers.reduce((a: number, b: number) => a + b);
   }
 }
