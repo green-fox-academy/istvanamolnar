3ced3fddd01dc0d74b5f0768a1d45e2cd84ed538
istvanamolnar <istvan0988@gmail.com>
2019-04-25 16:58:47 +0200

    (feat)generate an array of the secret numbers

---
+++ Foundation/Week4/Testing/CowsAndBulls/CowsAndBulls.ts
@@ -0,0 +1,15 @@
+'use strict';
+
+function randomArray(): number[] { //generates the array of the secret numbers
+  let arrayOfNumbers: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let cabArray: number[] = [];
+  for (let i: number = 0; i < 4; i++) {
+    let randomIndex: number = Math.floor(Math.random() * 10 - i);
+    randomIndex = randomIndex >= 0 ? randomIndex : -randomIndex;
+    cabArray.push(arrayOfNumbers[randomIndex]);
+    arrayOfNumbers.splice(arrayOfNumbers.indexOf(cabArray[i]), 1);    
+  }
+  return cabArray;
+}
+
+console.log(randomArray());
