f21807f3f9b8b67030bb531d335c26a1e14b06a7
istvanamolnar <istvan0988@gmail.com>
2019-04-25 19:45:28 +0200

    (feat) functions refactored

--- Foundation/Week4/Testing/CowsAndBulls/CowsAndBulls.ts
+++ Foundation/Week4/Testing/CowsAndBulls/CowsAndBulls.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-function secretArray(): number[] { //generates the array of the secret numbers
+function getSecretArray(): number[] { //generates the array of the secret numbers
   let arrayOfNumbers: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   let cabArray: number[] = [];
   for (let i: number = 0; i < 4; i++) {
@@ -11,19 +11,14 @@ function secretArray(): number[] { //generates the array of the secret numbers
   }
   return cabArray;
 }
-const secret: number[] = secretArray();
-console.log(secret);
-
-let prevGuess: string = '';
 function guesser(): number[] { //takes and prints guesses
   let readlineSync = require('readline-sync');
   let guess: number[] = readlineSync.question('Enter four single digit integers: ').split('').map((x:string) => parseInt(x));
-  prevGuess += '\n' + guess;
   return guess;
 }
-
 function cattleCounter(secret: number[]): any {
   const guess: number[] = guesser();
+  prevGuess += guess + '\n';
   let Cattle: any = {
     'bull': 0,
     'cow': 0
@@ -41,4 +36,7 @@ function cattleCounter(secret: number[]): any {
   return Cattle.bull === 4 ? console.log('GG WP') : cattleCounter(secret);
 }
 
+const secret: number[] = getSecretArray();
+console.log(secret);
+let prevGuess: string = '';
 cattleCounter(secret);
