549d802112dc570308f972498669304ff0fec7ca
istvanamolnar <istvan0988@gmail.com>
2019-04-27 19:58:10 +0200

    (feat) function improved

--- Foundation/TrialExam/exam-trial-basics/isSymmetric.ts
+++ Foundation/TrialExam/exam-trial-basics/isSymmetric.ts
@@ -1,11 +1,12 @@
 'use strict';
 
 function isSymmetric(matrix: number[][]): boolean {
-  let counter: number = 0;
   for (let i: number = 0; i < matrix.length; i++) {
     for (let j: number = matrix.length - 1; j >= 0; j--) {
-      matrix[i][j] != matrix[j][i] ? counter++ : false;
+      if (matrix[i][j] != matrix[j][i]) {
+        return false;
+      }
     }
   }
-  return counter === 0;
+  return true;
 }
