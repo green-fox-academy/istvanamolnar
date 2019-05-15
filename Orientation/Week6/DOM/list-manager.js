04a6bff3a4c0cdd532eb38f32b7fa8bbcfd8bb20
istvanamolnar <istvan0988@gmail.com>
2019-05-15 16:07:58 +0200

    DOM practice

--- Orientation/Week6/DOM/list-manager.js
+++ Orientation/Week6/DOM/list-manager.js
@@ -1,11 +1,56 @@
 'use strict';
 
-/* 1, Build up the necessary structure with JavaScript (try to use semantically correct elements)
-2, Achive the same design by css
-3, Try not to use css classes
-4, Add some functionality to the buttons
-    - If "Up" is clicked the selection should move up by one
-    - If "Down" is clicked the selection should move down by one
-    - If "X" is clicked the selected item should be removed and the first item should be selected
-    - If ">" is clicked the selected item should be moved to the right side and the first item on the left side should be selected
-5, Check all the edge cases, no error should be printed to the console */
+let move = () => {
+  let selectedp = document.getElementsByClassName("selectp")[0];
+  let node = document.createElement("P");
+  let havebox = document.getElementsByClassName("have")
+  node.appendChild(document.createTextNode(selectedp.innerHTML));
+  if (havebox[0].innerHTML.indexOf(selectedp.innerHTML) === -1) {
+    havebox[0].appendChild(node);
+  } else {
+    alert(`${node.innerText} is added already.`)
+  }
+}
+
+let down = () => {
+  let listItems = document.getElementsByClassName("need")[0].children;
+  let index;
+  for (let item in listItems) {
+    if (listItems[item].className === "selectp") {
+      index = item;
+      listItems[parseInt(item)].className = "";
+    }
+  }
+  if (index < listItems.length - 1) {
+    listItems[parseInt(index) + 1].className = "selectp";
+  } else {
+    listItems[0].className = "selectp";
+  }
+}
+
+let up = () => {
+  let listItems = document.getElementsByClassName("need")[0].children;
+  let index;
+  for (let item in listItems) {
+    if (listItems[item].className === "selectp") {
+      index = item;
+      listItems[parseInt(item)].className = "";
+    }
+  }
+  if (index > 0) {
+    listItems[parseInt(index) - 1].className = "selectp";
+  } else {
+    listItems[listItems.length - 1].className = "selectp";
+  }
+}
+
+let del = () => {
+  let selectedp = document.getElementsByClassName("selectp")[0].innerText;
+  let haveBox = document.getElementsByClassName("box have")[0].children;
+  for (let item in haveBox) {
+    if (haveBox[item].innerText === selectedp) {
+      return document.getElementsByClassName("box have")[0].removeChild(document.getElementsByClassName("box have")[0].children[item]);
+    }
+  }
+  alert(`${selectedp} is not on your list.`);
+}
