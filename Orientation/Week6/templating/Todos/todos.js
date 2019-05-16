6ab27a2dd574c267f7b7ecfbb2a651b7cf01ab43
istvanamolnar <istvan0988@gmail.com>
2019-05-16 15:24:54 +0200

    server basic practice

--- Orientation/Week6/templating/Todos/todos.js
+++ Orientation/Week6/templating/Todos/todos.js
@@ -2,21 +2,20 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// home page
 app.get('/', (req, res) => {
-  res.render('byname', {
-    title: 'Hello',
-    name: req.query.name || 'Guest',
+  res.render('todos', {
+    todos: [
+      'get up',
+      'code',
+      'eat',
+      'sleep',
+      'repeat'
+    ],
   });
 });
-const greetByName = () => {
-}
 
-
-// start express app on port 3000
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
