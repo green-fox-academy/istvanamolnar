18e1bdfcaa2ba8ec704e561eabcb80fd18b1ac8e
istvanamolnar <istvan0988@gmail.com>
2019-05-18 15:06:23 +0200

    (feat) arrow buttons work

--- Orientation/Week6/imageGallery/index.js
+++ Orientation/Week6/imageGallery/index.js
@@ -1,25 +1,22 @@
 'use strict';
 
 let Images = [
-  {title: 'Princess Cruise Ship', description: 'Huge passenger cruise ship sailing on the ocean in the golden hour.', url: 'images/Princess Cruise Ship.jpg'},
-  {title: 'Desert Sunset', description: 'A windy sunset in the desert from a different perspective', url: 'images/Desert Sunset.jpg'},
+  {title: 'Princess Cruise Ship', description: 'Huge passenger cruise ship sailing on the ocean in the golden hour.', url: 'images/PrincessCruiseShip.jpg'},
+  {title: 'Desert Sunset', description: 'A windy sunset in the desert from a different perspective', url: 'images/DesertSunset.jpg'},
   {title: 'Dome Interior', description: 'A dome of an iconic renaissance building', url: 'images/Dome.jpg'},
-  {title: 'Canadian Forest', description: 'Breathtaking moment in the North American deep forest with mountains above snow level.', url: 'images/Canadian Forest.jpg'},
-  {title: 'Long Exposure', description: 'A futuristic photo in a dark room with long expo and motion combo.', url: 'images/Long Exposure.jpg'},
-  {title: 'Beach Holiday', description: 'Beautiful day at the beach in Hawaii, photo of the most popular vintage van.', url: 'images/Beach Holiday.jpg'},
-  {title: 'Misty Morning', description: 'Spooky black and white photo before sunrising, makes you never want to go to this place.', url: 'images/Misty Morning.jpg'},
+  {title: 'Canadian Forest', description: 'Breathtaking moment in the North American deep forest.', url: 'images/CanadianForest.jpg'},
+  {title: 'Long Exposure', description: 'A futuristic photo in a dark room with long expo and motion combo.', url: 'images/LongExposure.jpg'},
+  {title: 'Beach Holiday', description: 'Beautiful day at the beach in Hawaii, photo of the most popular vintage van.', url: 'images/BeachHoliday.jpg'},
+  {title: 'Misty Morning', description: 'Spooky black and white photo before sunrising.', url: 'images/MistyMorning.jpg'},
 ];
 
 let loadImages = () => {
   for (let i = 0; i < Images.length; i++) {
     let gallery = document.getElementById("thumbnails");
-    let newLink = document.createElement("A");
-    newLink.href = "http://google.com";
     let newPicture = document.createElement("IMG");
     newPicture.src = Images[i].url;
     newPicture.className += "smallImage";
-    newLink.appendChild(newPicture);
-    gallery.appendChild(newLink);
+    gallery.appendChild(newPicture);
     if (i === 0) {newPicture.className += " currentImage";}
   }
 }
@@ -27,13 +24,10 @@ loadImages();
 
 let addImages = () => {
   let gallery = document.getElementById("thumbnails");
-  let newLink = document.createElement("A");
-  newLink.href = "http://google.com";
   let newPicture = document.createElement("IMG");
   newPicture.src = Images[Images.length - 1].url;
   newPicture.className += "smallImage";
-  newLink.appendChild(newPicture);
-  gallery.appendChild(newLink);
+  gallery.appendChild(newPicture);
 }
 
 let viewImage = () => {
@@ -42,14 +36,43 @@ let viewImage = () => {
   let newDescription = document.createElement("P");
   let bg = document.createElement("DIV");
   bg.className += "textbg";
-  newDescription.innerHTML = Images[2].description;
-  newTitle.innerHTML = Images[2].title;
-  newImage.src = Images[2].url;
+  let currentImage = document.getElementsByClassName("currentImage")[0];
+  let source = Images.filter(x => currentImage.src.includes(x.url))[0];
+  newDescription.innerHTML = source.description;
+  newTitle.innerHTML = source.title;
+  newImage.src = currentImage.src;
   newImage.id = "bigImage";
   let main = document.getElementById("bigImageContainer");
+  main.innerHTML = "";
   main.appendChild(newImage);
   main.appendChild(newTitle);
   main.appendChild(newDescription);
   main.appendChild(bg);
 }
 viewImage();
+
+let goRight = () => {
+  let currentImage = document.getElementsByClassName("currentImage")[0];
+  let indexCounter = Images.indexOf(Images.filter(x => currentImage.src.includes(x.url))[0]) + 2;
+  if (indexCounter <= Images.length) {
+    currentImage.className = "smallImage";
+    currentImage.nextSibling.className += " currentImage";
+  } else {
+    document.getElementById("thumbnails").firstChild.className += " currentImage";
+    document.getElementById("thumbnails").lastChild.className = "smallImage";
+  }
+  viewImage();
+}
+
+let goLeft = () => {
+  let currentImage = document.getElementsByClassName("currentImage")[0];
+  let indexCounter = Images.indexOf(Images.filter(x => currentImage.src.includes(x.url))[0]);
+  if (indexCounter > 0) {
+    currentImage.className = "smallImage";
+    currentImage.previousSibling.className += " currentImage";
+  } else {
+    document.getElementById("thumbnails").lastChild.className += " currentImage";
+    document.getElementById("thumbnails").firstChild.className = "smallImage";
+  }
+  viewImage();
+}
