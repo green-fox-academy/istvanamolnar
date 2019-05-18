0ebd1241a18f36d6ebe7e58942a7237c84f5ae39
istvanamolnar <istvan0988@gmail.com>
2019-05-18 11:22:26 +0200

    index.js created

---
+++ Orientation/Week6/imageGallery/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let Images = [
+  {title: 'Princess Cruise Ship', description: 'Huge passenger cruise ship sailing on the ocean in the golden hour.', url: 'images/Princess Cruise Ship.jpg'},
+  {title: 'Desert Sunset', description: 'A windy sunset in the desert from a different perspective', url: 'images/Desert Sunset.jpg'},
+  {title: 'Dome Interior', description: 'A dome of an iconic renaissance building', url: 'images/Dome.jpg'},
+  {title: 'Canadian Forest', description: 'Breathtaking moment in the North American deep forest with mountains above snow level.', url: 'images/Canadian Forest.jpg'},
+  {title: 'Long Exposure', description: 'A futuristic photo in a dark room with long expo and motion combo.', url: 'images/Long Exposure.jpg'},
+  {title: 'Beach Holiday', description: 'Beautiful day at the beach in Hawaii, photo of the most popular vintage van.', url: 'images/Beach Holiday.jpg'},
+  {title: 'Misty Morning', description: 'Spooky black and white photo before sunrising, makes you never want to go to this place.', url: 'images/Misty Morning.jpg'},
+]
