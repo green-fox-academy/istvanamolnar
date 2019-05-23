02eadff06ff3ff88db4f9203df54dc4965d6b2fe
istvanamolnar <istvan0988@gmail.com>
2019-05-23 10:55:18 +0200

    (feat) setup

--- Orientation/Week7/mysql_node/bookinfo/bookinfo.js
+++ Orientation/Week7/mysql_node/bookinfo/bookinfo.js
@@ -3,14 +3,14 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
-//const path = require('path'); // if UI => HTML
+const path = require('path'); // if UI => HTML
 const port = 3000;
 
 app.use(express.json());
 
 app.listen(port, () => console.log(`The server is up and running on ${port}!`)); // should be at the end of the code
 
-//app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html'))); // if UI => HTML
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html'))); // if UI => HTML
 
 app.get('/', function (req, res) { // no UI => DB
   res.send('Hello World');
