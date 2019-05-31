a73b8d4d3ef1dd65ee81965869b5cb784d821281
istvanamolnar <istvan0988@gmail.com>
2019-05-31 10:38:32 +0200

    (feat) search in db

--- Orientation/Week8/AJAX practice/public/index.js
+++ Orientation/Week8/AJAX practice/public/index.js
@@ -1,1 +1,78 @@
 'use strict';
+
+const bookIdRequest = new XMLHttpRequest();
+  bookIdRequest.open('GET', 'http://localhost:3000/fulldata', true);
+  bookIdRequest.send();
+  bookIdRequest.onload = (data) => {
+    const id = JSON.parse(data.target.response);
+    id.forEach(x => {
+      let oneRow= document.getElementsByTagName("tbody")[0].appendChild(document.createElement("TR"))
+      oneRow.appendChild(document.createElement("TD")).innerText = x.book_name;
+      oneRow.appendChild(document.createElement("TD")).innerText = x.aut_name;
+      oneRow.appendChild(document.createElement("TD")).innerText = x.cate_descrip;
+      oneRow.appendChild(document.createElement("TD")).innerText = x.pub_name;
+      oneRow.appendChild(document.createElement("TD")).innerText = `$ ${x.book_price}`;
+    });
+  };
+
+document.querySelector('thead').addEventListener('click', (event) => {
+  event.preventDefault();
+  clearTable();
+  if (event.target.innerText[0] === 'C') {
+    console.log(event) //.target.selectedOptions[0].innerText);
+  } else {
+    orderBy(event.target.href);
+  }
+});
+
+function orderBy(feature) {
+  const orderRequest = new XMLHttpRequest();
+  orderRequest.open('GET', `${feature}`, true);
+  orderRequest.send();
+  orderRequest.onload = (data) => {
+    const id = JSON.parse(data.target.response);
+    id.forEach(book => {
+      let oneRow= document.getElementsByTagName("tbody")[0].appendChild(document.createElement("TR"));
+      oneRow.appendChild(document.createElement("TD")).innerText = book.book_name;
+      oneRow.appendChild(document.createElement("TD")).innerText = book.aut_name;
+      oneRow.appendChild(document.createElement("TD")).innerText = book.cate_descrip;
+      oneRow.appendChild(document.createElement("TD")).innerText = book.pub_name;
+      oneRow.appendChild(document.createElement("TD")).innerText = `$ ${book.book_price}`;
+    });
+  };
+}
+
+function clearTable() {
+  const tableBody = document.querySelector('tbody');
+  while (tableBody.firstChild) {
+    tableBody.removeChild(tableBody.firstChild);
+  }
+}
+
+let select = document.querySelector('#category')
+select.addEventListener('click', () => {
+  for (let i = 0; i < select.children.length; i++) {
+    select.children[i].style.display = 'initial';
+    if (select.children[i].dataset.category !== select.value) {
+      console.log(select.children[i]);
+      console.log(select.value);
+      //select.children[i].style.display = 'none';
+    }
+  }
+});
+function filterByCategory() {
+  const bookIdRequest = new XMLHttpRequest();
+  bookIdRequest.open('GET', 'http://localhost:3000/fulldata', true);
+  bookIdRequest.send();
+  bookIdRequest.onload = (data) => {
+    const id = JSON.parse(data.target.response);
+    id.forEach(x => {
+      let oneRow= document.getElementsByTagName("tbody")[0].appendChild(document.createElement("TR"))
+      oneRow.appendChild(document.createElement("TD")).innerText = x.book_name;
+      oneRow.appendChild(document.createElement("TD")).innerText = x.aut_name;
+      oneRow.appendChild(document.createElement("TD")).innerText = x.cate_descrip;
+      oneRow.appendChild(document.createElement("TD")).innerText = x.pub_name;
+      oneRow.appendChild(document.createElement("TD")).innerText = `$ ${x.book_price}`;
+    });
+  };
+}
--- Orientation/Week8/AJAX practice/server.js
+++ Orientation/Week8/AJAX practice/server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
 const path = require('path');
 const port = 3000;
 
@@ -9,8 +10,113 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'))
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'istvan',
+  password: 'mysqlPractice',
+  database: 'bookinfo'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log(err.toString());
+    return;
+  }
+  console.log(`Connected to Database.`);
+});
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/newsletter.html'));
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/fulldata', (req, res) => {
+  connection.query(`SELECT book_name, aut_name, cate_descrip, pub_name, book_price 
+  FROM book_mast 
+  LEFT JOIN author ON book_mast.aut_id = author.aut_id 
+  LEFT JOIN category ON book_mast.cate_id = category.cate_id 
+  LEFT JOIN publisher ON book_mast.pub_id = publisher.pub_id
+  ORDER BY book_name;`, (err, rows) => {
+    if (err) {
+      console.log(err.toString());
+      return;
+    }
+    res.send(rows);
+  });
+});
+
+app.get('/bytitle', (req, res) => {
+  connection.query(`SELECT book_name, aut_name, cate_descrip, pub_name, book_price 
+  FROM book_mast 
+  LEFT JOIN author ON book_mast.aut_id = author.aut_id 
+  LEFT JOIN category ON book_mast.cate_id = category.cate_id 
+  LEFT JOIN publisher ON book_mast.pub_id = publisher.pub_id
+  ORDER BY book_name;`, (err, rows) => {
+    if (err) {
+      console.log(err.toString());
+      return;
+    }
+    res.send(rows);
+  });
+});
+
+app.get('/byauthor', (req, res) => {
+  connection.query(`SELECT book_name, aut_name, cate_descrip, pub_name, book_price 
+  FROM book_mast 
+  LEFT JOIN author ON book_mast.aut_id = author.aut_id 
+  LEFT JOIN category ON book_mast.cate_id = category.cate_id 
+  LEFT JOIN publisher ON book_mast.pub_id = publisher.pub_id
+  ORDER BY aut_name;`, (err, rows) => {
+    if (err) {
+      console.log(err.toString());
+      return;
+    }
+    res.send(rows);
+  });
+});
+
+app.get('/bycategory', (req, res) => {
+  connection.query(`SELECT book_name, aut_name, cate_descrip, pub_name, book_price 
+  FROM book_mast 
+  LEFT JOIN author ON book_mast.aut_id = author.aut_id 
+  LEFT JOIN category ON book_mast.cate_id = category.cate_id 
+  LEFT JOIN publisher ON book_mast.pub_id = publisher.pub_id
+  ORDER BY cate_descrip;`, (err, rows) => {
+    if (err) {
+      console.log(err.toString());
+      return;
+    }
+    res.send(rows);
+  });
+});
+
+app.get('/bypublisher', (req, res) => {
+  connection.query(`SELECT book_name, aut_name, cate_descrip, pub_name, book_price 
+  FROM book_mast 
+  LEFT JOIN author ON book_mast.aut_id = author.aut_id 
+  LEFT JOIN category ON book_mast.cate_id = category.cate_id 
+  LEFT JOIN publisher ON book_mast.pub_id = publisher.pub_id
+  ORDER BY pub_name;`, (err, rows) => {
+    if (err) {
+      console.log(err.toString());
+      return;
+    }
+    res.send(rows);
+  });
+});
+
+app.get('/byprice', (req, res) => {
+  connection.query(`SELECT book_name, aut_name, cate_descrip, pub_name, book_price 
+  FROM book_mast 
+  LEFT JOIN author ON book_mast.aut_id = author.aut_id 
+  LEFT JOIN category ON book_mast.cate_id = category.cate_id 
+  LEFT JOIN publisher ON book_mast.pub_id = publisher.pub_id
+  ORDER BY book_price;`, (err, rows) => {
+    if (err) {
+      console.log(err.toString());
+      return;
+    }
+    res.send(rows);
+  });
 });
 
 app.post('/signup',(req, res) => {
@@ -18,4 +124,4 @@ app.post('/signup',(req, res) => {
   res.json(req.body);
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Server is running on port ${port}.`));
