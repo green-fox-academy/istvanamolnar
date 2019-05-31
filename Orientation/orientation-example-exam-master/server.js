eb54b13a5f942ed621c4c6224df12ae263210f15
istvanamolnar <istvan0988@gmail.com>
2019-05-31 16:07:05 +0200

    (feat) basic file setup

---
+++ Orientation/orientation-example-exam-master/server.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const path = require('path');
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'))
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'istvan',
+  password: 'mysqlPractice',
+  database: 'bookinfo'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log(err.toString());
+    return;
+  }
+  console.log(`Connected to Database.`);
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
+app.listen(port, () => console.log(`Server is running on port ${port}.`));
