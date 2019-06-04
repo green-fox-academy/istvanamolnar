b46a81c27305d243f2f6405af4e3d9e291421d0c
istvanamolnar <istvan0988@gmail.com>
2019-06-04 17:12:55 +0200

    (feat) file setup

---
+++ Orientation/Week9/quiz applicaton/server.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const path = require('path');
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'istvan',
+  password: 'mysqlPractice',
+  database: 'aliasdb'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log(err.toString());
+    return;
+  }
+  console.log(`Connected to Database.`);
+});
+
+app.get('/game', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/game.html'));
+});
+
+app.get('/questions', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/questions.html'));
+});
+
+
+app.listen(port, () => console.log(`Server is running on port ${port}.`));
