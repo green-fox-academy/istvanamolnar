14772b8d1d76cf47dce8ee87601b52b038636579
Istvan Molnar <istvan0988@gmail.com>
2019-06-19 09:38:32 +0200

    (feat) API data received

--- Project/weather-app/src/app/app.module.ts
+++ Project/weather-app/src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { EnvServiceProvider } from './env.service.provider';
 import { HttpClientModule } from '@angular/common/http';
 
--- Project/weather-app/src/app/weather/weather.component.ts
+++ Project/weather-app/src/app/weather/weather.component.ts
@@ -1,18 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { EnvService } from '../env.service';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
   styleUrls: ['./weather.component.css']
 })
+
+@Injectable()
 export class WeatherComponent implements OnInit {
+  weatherData: any;
   constructor(private env: EnvService, private http: HttpClient) { 
   }
 
   ngOnInit() {
-    console.log(this.env.apiKey);
+    this.http.get(`http://api.openweathermap.org/data/2.5/weather?q=London&appid=${this.env.apiKey}`)
+    .subscribe((response: any) => {
+      this.weatherData = response;
+      console.log(this.weatherData.main.temp);
+    })
   }
-
+/* 
+  getWeatherInfo(): any {
+    
+  } */
 }
