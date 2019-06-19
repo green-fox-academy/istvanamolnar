194a7e853ed625aa65e1873ce71bd99128e225ed
Istvan Molnar <istvan0988@gmail.com>
2019-06-19 21:49:31 +0200

    chore

--- Project/weather-app/src/app/dashboard/dashboard.component.ts
+++ Project/weather-app/src/app/dashboard/dashboard.component.ts
@@ -14,7 +14,7 @@ export class DashboardComponent implements OnInit {
   cities: string[];
   cityData: City[];
   constructor(private info: GetWeatherInfoService, private http: HttpClient, private countryName: GetCountryNameService) { 
-    this.cities = ['Amsterdam', 'Basel', 'London', 'Porto', 'Toronto'];
+    this.cities = ['Amsterdam', 'Basel', 'London', 'Porto', 'Sofia'];
     this.cityData = [];
   }
 
